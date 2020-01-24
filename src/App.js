3b95bb44f7a7f52f1b7e5ef9a7f2108bda1b78a3
Tiago <hsbtiago@gmail.com>
2020-01-24 16:42:47 -0400

    Criação do projeto

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Grommet} from 'grommet';
+
+function App() {
+
+  const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '18px',
+        height: '20px',
+      },
+    },
+  };
+
+
+  return (
+    <Grommet theme={theme} full>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </Grommet>
+  );
+}
+
+export default App;
